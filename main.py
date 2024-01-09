1061d374eb7ad1323a3eadca39b843484e0b2f0a
vscheff <vonscheffler@protonmail.com>
2024-01-09 14:11:10 -0500

    Moved game logic out of main

--- main.py
+++ main.py
@@ -1,105 +1,126 @@
 from deck import Deck
 from players import get_moves, Player, Von
-from random import shuffle
+from random import seed, shuffle
 
+
+seed(69)
 
 VERBOSE_MODE = True
-GAMES = 1
-players = [Von() for _ in range(4)]
+GAMES = 200
+PLAYERS = [Von() for _ in range(4)]
 
 def main():
-	for _ in range(GAMES):
-		deck = Deck()
+    if VERBOSE_MODE:
+        play_game(True)
 
-		for _ in range(7):
-			for player in players:
-				player.draw(deck.draw())
+    for _ in range(GAMES - 1):
+        play_game(False)
 
-		shuffle(players)
+    print()
 
-		if VERBOSE_MODE:
-			print("Starting hands:")
-			for i in range(len(players)):
-				print(f"Player {i}: ", end='')
-				players[i].print_hand()
-			print()
+    for i in range(len(PLAYERS)):
+        print(f"Player {i} wins = {PLAYERS[i].wins}")
 
-		turn = len(players) - 1
-		turn_mod = 1
-		chosen_color = None
+def play_game(verbose):
+    deck = Deck()
 
-		deck.flip_top()
+    players = [i for i in PLAYERS]
 
-		if VERBOSE_MODE:
-			print(f"Starting with {deck.top}")
+    for _ in range(7):
+        for player in players:
+            player.draw(deck.draw())
 
-		while True:
-			turn = get_next_turn(turn, turn_mod)
-			current_player = players[turn]
+    shuffle(players)
 
-			if not (valid_moves := get_moves(deck.top, current_player.hand, chosen_color)):
-				drawn = deck.draw()
+    if verbose:
+        print("Starting hands:")
+        for i in range(len(players)):
+            print(f"Player {i}: ", end='')
+            players[i].print_hand()
+        print()
 
-				if VERBOSE_MODE:
-						print(f"Player {turn} draws {drawn}")
+    turn = len(players) - 1
+    turn_mod = 1
+    chosen_color = None
 
-				if not (valid_moves := get_moves(deck.top, [drawn], chosen_color)):
-					current_player.draw(drawn)
-					
-					continue
-				
-				played_card = drawn
-			else:
-				played_card = current_player.play(valid_moves)
+    deck.flip_top()
 
-			if VERBOSE_MODE:
-				print(f"Player {turn} plays {played_card}")
+    if verbose:
+        print(f"Starting with {deck.top}")
 
-			if not current_player.hand:
-				if VERBOSE_MODE:
-					print(f"\nPlayer {turn} wins!")
+    while True:
+        turn = get_next_turn(turn, turn_mod)
+        current_player = players[turn]
 
-				break
+        if verbose:
+            print(f"Player {turn} hand: ", end='')
+            current_player.print_hand()
 
-			if played_card.type == "skip":
-				turn = get_next_turn(turn, turn_mod)
-			elif played_card.type == "reverse":
-				turn_mod *= -1
-			elif played_card.type == "draw2":
-				next_player = get_next_turn(turn, turn_mod)
+        if not (valid_moves := get_moves(deck.top, current_player.hand, chosen_color)):
+            drawn = deck.draw()
 
-				for _ in range(2):
-					players[next_player].draw(deck.draw())
+            if verbose:
+                    print(f"\tPlayer {turn} draws {drawn}")
 
-				turn = get_next_turn(turn, turn_mod)
-			elif played_card.type == "wild":
-				chosen_color = player.chose_color()
+            if not (valid_moves := get_moves(deck.top, [drawn], chosen_color)):
+                current_player.draw(drawn)
+                
+                continue
+            
+            played_card = drawn
+        else:
+            played_card = current_player.play(valid_moves)
 
-				if VERBOSE_MODE:
-					print(f"\tPlayer {turn} chose {chosen_color}")
-			elif played_card.type == "wild+4":
-				chosen_color = player.chose_color()
+        if verbose:
+            print(f"\tPlayer {turn} plays {played_card}")
 
-				if VERBOSE_MODE:
-					print(f"\tPlayer {turn} chose {chosen_color}")
+        if not current_player.hand:
+            current_player.wins += 1
 
-				next_player = get_next_turn(turn, turn_mod)
+            if verbose:
+                print(f"\nPlayer {turn} wins!")
 
-				for _ in range(4):
-					players[next_player].draw(deck.draw())
+            break
 
-				turn = get_next_turn(turn, turn_mod)
+        if played_card.type == "skip":
+            turn = get_next_turn(turn, turn_mod)
+        elif played_card.type == "reverse":
+            turn_mod *= -1
+        elif played_card.type == "draw2":
+            next_player = get_next_turn(turn, turn_mod)
 
-			deck.play(played_card)
+            for _ in range(2):
+                players[next_player].draw(deck.draw())
+
+            turn = get_next_turn(turn, turn_mod)
+        elif played_card.type == "wild":
+            chosen_color = current_player.chose_color()
+
+            if verbose:
+                print(f"\tPlayer {turn} chose {chosen_color}")
+        elif played_card.type == "wild+4":
+            chosen_color = current_player.chose_color()
+
+            if verbose:
+                print(f"\tPlayer {turn} chose {chosen_color}")
+
+            next_player = get_next_turn(turn, turn_mod)
+
+            for _ in range(4):
+                players[next_player].draw(deck.draw())
+
+            turn = get_next_turn(turn, turn_mod)
+
+        deck.play(played_card)
 
 def get_next_turn(turn, turn_mod):
-	turn += turn_mod
+    turn += turn_mod
 
-	if turn == -1:
-		return len(players) - 1
+    if turn == -1:
+        return len(PLAYERS) - 1
 
-	return turn % len(players)
+    return turn % len(PLAYERS)
 
 
 if __name__ == "__main__":
-	main()
+    main()
--- players.py
+++ players.py
@@ -1,65 +1,77 @@
 from collections import defaultdict
+from deck import COLORS
 from operator import attrgetter
+from random import choice
 
 
 class Player:
-	def __init__(self):
-		self.hand = []
+    def __init__(self):
+        self.hand = []
+        self.wins = 0
 
-	def clear(self):
-		self.hand = []
+    def clear(self):
+        self.hand = []
 
-	def draw(self, card):
-		self.hand.append(card)
-		self.hand.sort()
+    def draw(self, card):
+        if card:
+            self.hand.append(card)
+            self.hand.sort()
 
-	def play(self, valid_moves):
-		raise NotImplementedError
+    def play(self, valid_moves):
+        raise NotImplementedError
 
-	def chose_color(self):
-		raise NotImplementedError
+    def chose_color(self):
+        raise NotImplementedError
 
-	def print_hand(self):
-		print(", ".join(str(card) for card in self.hand))
+    def print_hand(self):
+        print(", ".join(str(card) for card in self.hand))
+
+    def count_colors(self):
+        color_counts = defaultdict(int)
+
+        for card in self.hand:
+            if card.color:
+                color_counts[card.color] += 1
+        
+        return color_counts
 
 
 class Von(Player):
-	def play(self, valid_moves):
-		color_counts = defaultdict(int)
-		for card in self.hand:
-			color_counts[card.color] += 1
+    def play(self, valid_moves):
+        if not (color_counts := self.count_colors()):
+            self.hand.remove(valid_moves[0])
+            return valid_moves[0]
 
-		largest_color = max(color_counts, key=color_counts.get)
+        largest_color = max(color_counts, key=color_counts.get)
 
-		for card in valid_moves:
-			if card.color == largest_color:
-				self.hand.remove(card)
-				return card
+        for card in valid_moves:
+            if card.color == largest_color:
+                self.hand.remove(card)
+                return card
 
-		self.hand.remove(valid_moves[0])
-		return valid_moves[0]
+        self.hand.remove(valid_moves[0])
+        return valid_moves[0]
 
-	def chose_color(self):
-		color_counts = defaultdict(int)
-		for card in self.hand:
-			color_counts[card.color] += 1
+    def chose_color(self):
+        if not (color_counts := self.count_colors()):
+            return choice(COLORS)
 
-		return max(color_counts, key=color_counts.get)
+        return max(color_counts, key=color_counts.get)
 
 
 def get_moves(top, hand, chosen_color):
-	valid = []
+    valid = []
 
-	for card in hand:
-		if card.color is None:
-			if card.type == "wild" or not valid:
-				valid.append(card)
-		elif card.type == top.type:
-			valid.append(card)
-		elif top.color:
-			if card.color == top.color:
-				valid.append(card)
-		elif chosen_color == card.color:
-			valid.append(card)
+    for card in hand:
+        if card.color is None:
+            if card.type == "wild" or not valid:
+                valid.append(card)
+        elif card.type == top.type:
+            valid.append(card)
+        elif top.color:
+            if card.color == top.color:
+                valid.append(card)
+        elif chosen_color == card.color:
+            valid.append(card)
 
-	return valid
+    return valid
